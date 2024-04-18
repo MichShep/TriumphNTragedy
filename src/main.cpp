71acdd88de4281d5be25cb9780867b772efcb728
Michael Shepard <michshep@umich.edu>
2024-04-17 20:59:14 -0400

    Simple Window
    Watched a quick tutorial on how to make a basic window

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,93 @@ using std::endl;
 using std::cout;
 using std::string;
 
-int main(){
+//Graphics
+const int WINDOW_WIDTH = 512;
+const int WINDOW_HEIGHT = 284;
+SDL_Window* g_main_window;
+SDL_Renderer* g_main_renderer;
+
+//Colors
+namespace Colors{
+    const SDL_Color BLACK = {0, 0, 0, SDL_ALPHA_OPAQUE};
+}
+
+static bool Init(){
     if (SDL_Init(SDL_INIT_EVERYTHING) > 0){
         cout << "SDL_Init failed with error: " << SDL_GetError() << endl;
         return EXIT_FAILURE;
     }
 
-    string greetings = "Hello SDL2";
-    cout << greetings << endl;
+    g_main_window = SDL_CreateWindow(
+        "SDL Create Window (512x284)",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT, 
+        SDL_WINDOW_OPENGL
+    );
 
-    SDL_Quit(); //prevent memorty leak
+    if (g_main_window == nullptr){
+        cout << "Unable to create window. Error: "<< SDL_GetError() << endl;
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
+    g_main_renderer = SDL_CreateRenderer(g_main_window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+    return true;
+}
+
+void Shutdown(){
+    if (g_main_window != nullptr) {
+        SDL_DestroyWindow(g_main_window);
+        g_main_window = nullptr;
+    }
+
+    if (g_main_renderer != nullptr) {
+        SDL_DestroyRenderer(g_main_renderer);
+        g_main_renderer = nullptr;
+    }
+
+    SDL_Quit();
+}
+
+static void ClearScreen(SDL_Renderer* renderer){
+    SDL_SetRenderDrawColor(renderer, Colors::BLACK.r, Colors::BLACK.g, Colors::BLACK.b, Colors::BLACK.a);
+    SDL_RenderClear(renderer);
+
+}
+
+int main(){
+    if (Init() == false){
+        Shutdown();
+    }
+
+    //Draw Loop
+    SDL_Event event;
+    bool running = true;
+
+    while (running){
+        ClearScreen(g_main_renderer);
+
+        //Check and process I/O
+        if (SDL_PollEvent(&event)){
+            switch (event.type){
+                case SDL_KEYDOWN: {
+                    running = event.key.keysym.scancode != SDL_SCANCODE_ESCAPE;
+                    break;
+                }
+                case SDL_QUIT: {
+                    running = false;
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+        // Update the screen with the content rendered in the background buffer
+        SDL_RenderPresent(g_main_renderer);
+    }
+    Shutdown();
     return EXIT_SUCCESS;
 }
